173b9f3ae1d7208d741ba692824e2a0ada0fb16c
jesuscmdev <g.zuscm@gmail.com>
2021-05-25 08:43:18 -0500

    gsap  initial

--- wc_cart_promo.php
+++ wc_cart_promo.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /* 
 Plugin Name: wc_cart_promo
 Plugin URI: https://alpha.jesuscm.dev
@@ -8,3 +8,12 @@ Author: Jesus Cortes
 Author URI: https://jesuscm.dev
 License: GPLv2 
 */
+
+// param 1: hook, param 2: function, param 3: prioridad, param 4: parametros de la funcion
+
+add_action('wp_head', 'add_gsap', 10);
+
+function add_gsap()
+{
+    echo "<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js'></script>";
+}
