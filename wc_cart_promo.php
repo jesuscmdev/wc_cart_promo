dca0f7757de897db0e2f570c5afafa30174766bd
jesuscmdev <g.zuscm@gmail.com>
2021-05-25 08:38:01 -0500

    Initial plugin

---
+++ wc_cart_promo.php
@@ -0,0 +1,10 @@
+<?php 
+/* 
+Plugin Name: wc_cart_promo
+Plugin URI: https://alpha.jesuscm.dev
+Description: Agregar una promoción en base al número de pedidos de un usuario WC
+Version: 1.0 
+Author: Jesus Cortes
+Author URI: https://jesuscm.dev
+License: GPLv2 
+*/
