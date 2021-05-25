c4551f44576c2842de15f55751acbe0dd569513a
jesuscmdev <g.zuscm@gmail.com>
2021-05-25 10:51:48 -0500

    precio 0

--- wc_cart_promo.php
+++ wc_cart_promo.php
@@ -11,9 +11,51 @@ License: GPLv2
 
 // param 1: hook, param 2: function, param 3: prioridad, param 4: parametros de la funcion
 
-add_action('wp_head', 'add_gsap', 10);
+add_filter('woocommerce_get_price', 'aplica_precio_especial', 10, 2);
 
-function add_gsap()
+function aplica_precio_especial($price, $product)
 {
-    echo "<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js'></script>";
+
+    // if (!is_user_logged_in()) return $price;
+
+    // Listado de productos con precio especial
+    $product_list = array();
+
+    // % Descuento a aplicar
+    $discount = 10;
+
+    // Comprueba si el producto actual pertenece a la lista
+    if (in_array($product->id, $product_list) || empty($product_list)) {
+
+        // Comprueba si el usuario tiene precio especial
+        // if (usuario_con_precio_especial('cliente_vip')) {
+        //     $price = $price * (100 - $discount) / 100;
+        // }
+
+
+    }
+    $price = 0;
+    return $price;
+}
+
+function usuario_con_precio_especial($role = '', $user_id = null)
+{
+
+    if (is_numeric($user_id)) {
+        $user = get_user_by('id', $user_id);
+    } else {
+        $user = wp_get_current_user();
+    }
+
+    if (empty($user)) {
+        return false;
+    }
+    $args = array(
+        'customer_id' => $user
+    );
+    $orders = wc_get_orders($args);
+
+
+    // return in_array($role, (array) $user->roles);
+    return $orders;
 }
